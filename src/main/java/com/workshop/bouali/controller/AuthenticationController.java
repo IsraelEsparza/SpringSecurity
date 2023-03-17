53420238e217a753f50afcdad127d8fd8a70bbde
IsraelEsparza <isra912747@gmail.com>
2023-03-17 15:31:34 -0700

    Spring Boot - Security

---
+++ src/main/java/com/workshop/bouali/controller/AuthenticationController.java
@@ -0,0 +1,38 @@
+package com.workshop.bouali.controller;
+
+import com.workshop.bouali.config.JwtUtils;
+import com.workshop.bouali.dao.UserDao;
+import com.workshop.bouali.dto.AuthenticateResquest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    private final AuthenticationManager authenticationManager;
+    private final UserDao userDao;
+    private final JwtUtils jwtUtils;
+    @PostMapping("/authenticate")
+    public ResponseEntity<String> authenticate(
+            @RequestBody AuthenticateResquest request
+    ){
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
+        );
+        final UserDetails user = userDao.findUserByEmail(request.getEmail());
+        if (user != null){
+            return ResponseEntity.ok(jwtUtils.generateToken(user));
+        }
+        return ResponseEntity.status(400).body("Some error has occurred");
+    }
+}
---
+++ src/main/java/com/workshop/bouali/dao/UserDao.java
@@ -0,0 +1,37 @@
+package com.workshop.bouali.dao;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Repository;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+@Repository
+public class UserDao {
+
+    private final static List<UserDetails> APLICATION_USERS = Arrays.asList(
+            new User(
+                    "isra@gmail.com",
+                    "password",
+                    Collections.singleton(new SimpleGrantedAuthority("ROLE_ADMIN"))
+            ),
+            new User(
+                    "user@gmail.com",
+                    "password",
+                    Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"))
+            )
+    );
+
+    public UserDetails findUserByEmail(String email){
+        return APLICATION_USERS
+                .stream()
+                .filter(u -> u.getUsername().equals(email))
+                .findFirst()
+                .orElseThrow(()-> new UsernameNotFoundException("No user was found"))
+                ;
+    }
+}
